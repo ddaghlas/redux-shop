5289e2faa1e6d2da110faf2ec9ff2abd2ad65993
Diana Daghlas <dianadaghlas@yahoo.com>
2021-09-14 18:10:10 -0400

    Refactor Cart and cartItem components

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/react-hooks'; // Importing hooks
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -40,7 +41,7 @@ const Cart = () => {
 
   function calculateTotal() {
     let sum = 0;
-    state.cart.forEach((item) => {
+    state.cart.forEach(item => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
@@ -78,18 +79,18 @@ const Cart = () => {
       <h2>Shopping Cart</h2>
       {state.cart.length ? (
         <div>
-          {state.cart.map((item) => (
+          {state.cart.map(item => (
             <CartItem key={item._id} item={item} />
           ))}
 
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
 
-            {Auth.loggedIn() ? (
+            {Auth.loggedIn() ? 
               <button onClick={submitCheckout}>Checkout</button>
-            ) : (
+             : 
               <span>(log in to check out)</span>
-            )}
+            }
           </div>
         </div>
       ) : (
